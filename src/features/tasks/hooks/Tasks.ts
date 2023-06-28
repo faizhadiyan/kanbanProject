03c32d94f33a7d8c342e4dd131c92a95965b18c0
Faiz <faizhadiyanfirza@gmail.com>
2023-06-28 23:06:22 +0700

    needed to create function to update task progress

--- src/features/tasks/hooks/Tasks.ts
+++ src/features/tasks/hooks/Tasks.ts
@@ -5,6 +5,9 @@ import { TASK_PROGRESS_ID } from '../../../constants/app'
 
 interface useTaskActionType {
   completeTask: (taskId: number) => void
+  moveTaskCard: (taskId: number, directionNumber: 1 | -1) => void
+  // Ditambahkan
+  addTask: (title: string, detail: string, dueDate: string, progressOrder: number) => void
 }
 
 // Dalam hal ini, kita mendefinisikan hook bernama useTasksAction dan ini akan menghasilkan nilai return berupa function yang akan memanipulasi state dari task-task (yaitu: complete, add, delete, edit ...).
@@ -18,6 +21,13 @@ export const useTasksAction = (): useTaskActionType => {
     )
     setTasks(updatedTasks)
   }
+
+  // const moveTaskCard = (taskId: number, directionNumber: 1 | -1): void => {
+  //   const updatedTasks: Task[] = tasks.map((task) =>
+  //     task.id === taskId ? { ...task, progressOrder: TASK_PROGRESS_ID.COMPLETED } : task
+  //   )
+  //   setTasks(updatedTasks)
+  // }
 
   return {
     completeTask,
